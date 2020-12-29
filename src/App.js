00a6f3595f250e1ab789da54eca61350b552056e
Phu Nguyen <phu21994@gmail.com>
2020-12-29 23:44:18 +0700

    create random quote include: text, author, social icon, button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='quote-box'>
+      <div id='text'>
+      <h1 id="new-quote">Definiteness of purpose is the starting point of all achievement.</h1>
+      <p id="author">- W. Clement Stone</p>
+      <a href="twitter.com/intent/tweet" id="tweet-quote"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+      <a href="https://facebook.com/"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>  
+      
+
+      
+      
+      </div>
+         
+      
+
+      <Button />    
     </div>
   );
 }
