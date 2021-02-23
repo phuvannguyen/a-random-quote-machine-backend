8ea025413ff09f8c9ed1645037482811ca8ce65b
Phu Nguyen <phu21994@gmail.com>
2021-02-23 23:20:28 +0700

    Add Providers to wrapp all props from store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import store from "./redux/store.js"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>, document.getElementById('root'))
+};
+render();
+store.subscribe(render);
+reportWebVitals();
+function fancyLog() {
+  console.log("%c Rendered with", "background: purple; color: #FFF");
+  console.log(store.getState());
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
